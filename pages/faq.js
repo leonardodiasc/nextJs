9742ff65a06d7ecd408f00f30d4c38dce6b7f614
leonardodiasc <leonardodiass@gmail.com>
2023-03-07 15:43:39 -0300

    added Link

---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+export default function FAQ(){
+    return(
+        <div>
+            <h1>Alura Cases - Home</h1>
+            <a href="/">Ir para home</a>
+        </div>
+
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function HomePage(){
     return(
         <div>
             <h1>Alura Cases - Home</h1>
+            <Link href="/faq">
+                <div>Ir para o FAQ</div>
+            </Link>
         </div>
 
     )
