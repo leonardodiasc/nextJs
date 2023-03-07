83d5ab22f0462b952126d66c7f9fd0044375f0e9
leonardodiasc <leonardodiass@gmail.com>
2023-03-07 15:47:12 -0300

    added link to faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+
 export default function FAQ(){
     return(
         <div>
-            <h1>Alura Cases - Home</h1>
-            <a href="/">Ir para home</a>
+            <h1>Alura Cases - FAQ</h1>
+            <Link href="/">
+                <div>Ir para home</div>
+            </Link>
         </div>
 
     )
