9924c2d451011db6842bb712fb4c41f46e64b2a5
leonardodiasc <leonardodiass@gmail.com>
2023-03-08 09:49:25 -0300

    added Title function

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Link from 'next/link'
+import React from 'react';
 
+function Title({children}){
+    return(
+        <React.Fragment>
+            <h1>{children}</h1>
+            <style jsx>{`
+            h1{
+                    color: red;
+            }
+            `}</style>
+        </React.Fragment>
+    );
+}
 export default function HomePage(){
     return(
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title>Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para o FAQ
             </Link>
